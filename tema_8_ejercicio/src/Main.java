a71e414bec53e31bbd8c5ae628e93f57f8d86e2e
Adrian Jimenez Nieto <adrianjimeneznieto@noreplay.com>
2023-01-26 21:08:35 +0100

    introduzco clase Persona

--- tema_8_ejercicio/src/Main.java
+++ tema_8_ejercicio/src/Main.java
@@ -1,3 +1,29 @@
+
 public class Main {
-    
+    public static class Persona {
+
+        // Atributos de clase
+        private int edad;
+        private int telefono;
+        private String nombre;
+        public String getNombre(){
+            return nombre;
+        }
+        public int getEdad(){
+            return edad;
+        }
+        public int getTelefono(){
+            return telefono;
+        }
+        public void setEdad(int edad) {
+            this.edad = edad;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;
+        }
+    }
+
 }
